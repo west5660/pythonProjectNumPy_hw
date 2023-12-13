58be0cecb8b30acc51bb3402d521aba53f7c5449
west5660 <westw_56@mail.ru>
2023-12-13 22:10:56 +0300

    hw_and_cw_72_pandas

---
+++ 888.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+dframe = pd.read_csv('students.csv')
+sred = dframe[['Математика','Физика','Химия','Информатика','История']].mean(axis=1).round(2)
+dframe['Средний']=sred
+print(sred)
+dframe = dframe.sort_values('Средний', ascending=False)
+
+dframe.to_excel('exel.xlsx', index=False)   #сохранили новый ексель файл
+
+print(dframe['Средний'].max())
+print(dframe['Средний'].idxmax())
+maxball = dframe['Средний'].max()
+newf = dframe[dframe['Средний']==maxball]  #фильтр по количеству максимального балла
+print(newf['Имя'])
+minball = dframe['Средний'].min()
+newf1 = dframe[dframe['Средний']==minball]
+print(newf1['Имя'])
---
+++ 999.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+dframe = pd.read_excel('movies.xlsx')
+drama = dframe[dframe['Жанр'] == 'драма']
+spielberg = dframe[dframe['Режиссер'] == 'Стивен Спилберг']
+rating = dframe['Рейтинг'].max()
+top_rating = dframe[dframe['Рейтинг'] == rating]
+year = dframe[dframe['Год'] >= 2005]
+print('########################################### Драмма')
+print(drama)
+print('########################################### Спилберг')
+print(spielberg)
+print('########################################### Рейтинг')
+print(top_rating)
+print('########################################### С 2005 года')
+print(year)
